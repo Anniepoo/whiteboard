11250938250a161ab9c525ba9f0b9bc3379651be
Anniepoo <annie66us@yahoo.com>
2014-08-12 19:39:54 -0700

    Working, just drops items.  Moved most javascript to whiteboard.js

--- demo_whiteboard.pl
+++ demo_whiteboard.pl
@@ -158,133 +158,13 @@ whiteboard_body -->
 %	handles events on the websocket.
 
 script -->
+	html_requires(js('whiteboard.js')),
 	{ http_link_to_id(chat_websocket, [], WebSocketURL)
 	},
 	js_script({|javascript(WebSocketURL)||
-var whiteboard ;
-
 $(document).ready(function() {
-
-    whiteboard = {
-	pengine: undefined,
-
-	currentTool: "oval",
-
-	connection: undefined,
-
-	writeln: function(string) {
-		$('#output').append(string + "<br />")
-	},
-
-	unchoose_tools: function() {
-		$("#whiteboard .componentbar IMG").removeClass("selected");
-	},
-
-	newElement: function(e) {
-			$("#msg").text("down " + e.clientX + " " + e.clientY);
-	},
-
-	newElementMoveOrDrag: function(e) {
-		if (mouseDownCount === 0)
-			return;
-
-		var x = e.offsetX;
-		var y = e.offsetY;
-
-		$("#msg").text("drag " + x + " " + y + " " + e.button);
-	},
-
-	newElementCommit: function(e) {
-		$("#msg").text("commit " + e.offsetX + " " + e.offsetY);
-		whiteboard.sendChat("commit(" + whiteboard.currentTool + ", " + e.offsetX +
-		                ", " + e.offsetY + ")");
-	},
-
-	openWebSocket: function() {
-	      connection = new WebSocket("ws://"+
-				window.location.host+WebSocketURL,
-			     ['echo']);
-
-	      connection.onerror = function (error) {
-                  console.log('WebSocket Error ' + error);
-              };
-
-              connection.onmessage = function (e) {
-		console.log(e.data);
-		var data = eval('(' + e.data + ')');
-		var cmd = data.args[0];
-		var x = data.args[1];
-		var y = data.args[2];
-		var canvas = $("#whiteboard .drawarea").get(0);
-                var context = canvas.getContext('2d');
-
-                context.setTransform(1, 0, 0, 1, 0, 0);
-                context.beginPath();
-		switch (cmd) {
-	        case  "rect":
-			   context.rect(x-50, y-37.5, 100, 75);
-			   break;
-	        case  "oval":
-			   context.arc(x, y, 37.5, 0, Math.PI * 2);
-			   break;
-		case  "diamond":
-			   context.moveTo(x-50, y);
-			   context.lineTo(x, y-37.5);
-			   context.lineTo(x+50,y);
-			   context.lineTo(x, y+37.5);
-			   context.closePath();
-			   break;
-		default:
-			   break;
-		}
-
-                context.fillStyle = 'yellow';
-                context.fill();
-                context.lineWidth = 3;
-                context.strokeStyle = 'black';
-                context.stroke();
-	      };
-	},
-
-        sendChat: function(msg) {
-              connection.send(msg);
-        }
-    };
-
-    $("#rect_tool").on("mouseup", function() {
-	   whiteboard.unchoose_tools();
-	   whiteboard.currentTool = "rect";
-	   $("#rect_tool").addClass("selected");
-    });
-    $("#oval_tool").on("mouseup", function() {
-	   whiteboard.unchoose_tools();
-	   whiteboard.currentTool = "oval";
-	   $("#oval_tool").addClass("selected");
-    });
-    $("#diamond_tool").on("mouseup", function() {
-	   whiteboard.unchoose_tools();
-	   whiteboard.currentTool = "diamond";
-	   $("#diamond_tool").addClass("selected");
-    });
-
-    $("#whiteboard .drawarea").on(
-	      {	"mousedown": whiteboard.newElement,
-		"mousemove": whiteboard.newElementMoveOrDrag,
-		"mouseup": whiteboard.newElementCommit});
-
-    whiteboard.openWebSocket();
+    ws_initialize(WebSocketURL);
 });
-
-var mouseDown = [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    mouseDownCount = 0;
-document.body.onmousedown = function(evt) {
-  ++mouseDown[evt.button];
-  ++mouseDownCount;
-}
-document.body.onmouseup = function(evt) {
-  --mouseDown[evt.button];
-  --mouseDownCount;
-}
 		  |}).
 
 %%	accept_chat(+WebSocket) is det.
@@ -333,7 +213,7 @@ handle_message(Message, _Room) :-
 	chatroom{joined:Id} :< Message, !,
 	assertz(visitor(Id)),
 	forall(utterance(Utterance),
-	       chatroom_send(Id, Utterance)).
+	       chatroom_send(Id, text(Utterance))).
 handle_message(Message, _Room) :-
 	chatroom{left:Id} :< Message, !,
 	retractall(visitor(Id)).
--- js/whiteboard.js
+++ js/whiteboard.js
@@ -1,30 +1,14 @@
-/* JavaScript for the Whiteboard application */
-var whiteboard = {
+
+var whiteboard = {};
+
+var ws_initialize = function(WebSocketURL) {
+
+    whiteboard = {
 	pengine: undefined,
-	
-	currentTool: "rect",
-	
-	startEngine: function() {
-		this.pengine = new Pengine({
-			application: 'whiteboard',
-			ask: "new_whiteboard(2342353)",
-			onsuccess: function() {
-				whiteboard.writeln("success " + JSON.stringify(this.data));
-			},
-			onfailure: function() {
-				whiteboard.writeln("failure");
-			},
-			onstop: function() {
-				whiteboard.writeln("stopped");
-			},
-			onabort: function() {
-				whiteboard.writeln("abort");
-			},
-			onerror: function() {
-				whiteboard.writeln("error" + JSON.stringify(this.data));
-			}
-		});
-	},
+
+	currentTool: "oval",
+
+	connection: undefined,
 
 	writeln: function(string) {
 		$('#output').append(string + "<br />")
@@ -41,52 +25,104 @@ var whiteboard = {
 	newElementMoveOrDrag: function(e) {
 		if (mouseDownCount === 0)
 			return;
-			
-		var x = e.offsetX; 
-		var y = e.offsetY; 
-		
+
+		var x = e.offsetX;
+		var y = e.offsetY;
+
 		$("#msg").text("drag " + x + " " + y + " " + e.button);
 	},
 
 	newElementCommit: function(e) {
-		$("#msg").text("commit " + e.clientX + " " + e.clientY);
-		this.pengine.respond("commit(" + this.currentTool + ", " + e.clientX + 
-		                ", " + e.clientY + ")");
-	}
-}
+		$("#msg").text("commit " + e.offsetX + " " + e.offsetY);
+		whiteboard.sendChat("commit(" + whiteboard.currentTool + ", " + e.offsetX +
+		                ", " + e.offsetY + ")");
+	},
 
-$(document).ready(function() {
-	whiteboard.startEngine();
-	
-	$("#rect_tool").on("click", function() {
-		whiteboard.unchoose_tools();
-		whiteboard.currentTool = "rect";
-		$("#rect_tool").addClass("selected");
-	});
-	$("#oval_tool").on("click", function() {
-		whiteboard.unchoose_tools();
-		whiteboard.currentTool = "oval";
-		$("#oval_tool").addClass("selected");
-	});
-	$("#diamond_tool").on("click", function() {
-		whiteboard.unchoose_tools();
-		whiteboard.currentTool = "diamond";
-		$("#diamond_tool").addClass("selected");
-	});
+	openWebSocket: function() {
+	      connection = new WebSocket("ws://"+
+				window.location.host+WebSocketURL,
+			     ['echo']);
 
-	$("#whiteboard .drawarea").on(
-					{	"mousedown": whiteboard.newElement,
-						"mousemove": whiteboard.newElementMoveOrDrag,
-						"mouseup": whiteboard.newElementCommit});
-});
+	      connection.onerror = function (error) {
+                  console.log('WebSocket Error ' + error);
+              };
+
+              connection.onmessage = function (e) {
+		console.log(e.data);
+		var data = eval('(' + e.data + ')');
+		var cmd = data.args[0];
+		var x = data.args[1];
+		var y = data.args[2];
+		var canvas = $("#whiteboard .drawarea").get(0);
+                var context = canvas.getContext('2d');
+
+                context.setTransform(1, 0, 0, 1, 0, 0);
+                context.beginPath();
+		switch (cmd) {
+	        case  "rect":
+			   context.rect(x-50, y-37.5, 100, 75);
+			   break;
+	        case  "oval":
+			   context.arc(x, y, 37.5, 0, Math.PI * 2);
+			   break;
+		case  "diamond":
+			   context.moveTo(x-50, y);
+			   context.lineTo(x, y-37.5);
+			   context.lineTo(x+50,y);
+			   context.lineTo(x, y+37.5);
+			   context.closePath();
+			   break;
+		default:
+			   break;
+		}
+
+                context.fillStyle = 'yellow';
+                context.fill();
+                context.lineWidth = 3;
+                context.strokeStyle = 'black';
+                context.stroke();
+	      };
+	},
+
+        sendChat: function(msg) {
+              connection.send(msg);
+        }
+    };
+
+    $("#rect_tool").on("mouseup", function() {
+	   whiteboard.unchoose_tools();
+	   whiteboard.currentTool = "rect";
+	   $("#rect_tool").addClass("selected");
+    });
+    $("#oval_tool").on("mouseup", function() {
+	   whiteboard.unchoose_tools();
+	   whiteboard.currentTool = "oval";
+	   $("#oval_tool").addClass("selected");
+    });
+    $("#diamond_tool").on("mouseup", function() {
+	   whiteboard.unchoose_tools();
+	   whiteboard.currentTool = "diamond";
+	   $("#diamond_tool").addClass("selected");
+    });
+
+    $("#whiteboard .drawarea").on(
+	      {	"mousedown": whiteboard.newElement,
+		"mousemove": whiteboard.newElementMoveOrDrag,
+		"mouseup": whiteboard.newElementCommit});
+
+    whiteboard.openWebSocket();
+};
 
 var mouseDown = [0, 0, 0, 0, 0, 0, 0, 0, 0],
     mouseDownCount = 0;
-document.body.onmousedown = function(evt) { 
-  ++mouseDown[evt.button];
-  ++mouseDownCount;
-}
-document.body.onmouseup = function(evt) {
-  --mouseDown[evt.button];
-  --mouseDownCount;
-}
+	
+$(document).ready( function() {
+	document.body.onmousedown = function(evt) {
+	  ++mouseDown[evt.button];
+	  ++mouseDownCount;
+	};
+	document.body.onmouseup = function(evt) {
+	  --mouseDown[evt.button];
+	  --mouseDownCount;
+	};
+});
